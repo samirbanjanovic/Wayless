f376191dbb308acc9eb2412d0707581d8ed89c93
Samir Banjanovic <sbanj17@gmail.com>
2018-01-18 10:55:56 -0500

    Fixed spelling , removed constructor that was able to take SourceObject, and added comments. Doing so made the usage less confusing and cleaner code. Added documentation above all public properties. Added basic comments throughout code

--- src/Wayless/Core/IWaylessMap.cs
+++ src/Wayless/Core/IWaylessMap.cs
@@ -15,11 +15,9 @@ namespace Wayless
         IWaylessMap<TDestination, TSource> FieldSet(Expression<Func<TDestination, object>> destinationExpression, object fieldValue);
         IWaylessMap<TDestination, TSource> FieldSkip(Expression<Func<TDestination, object>> ignoreAtDestinationExpression);
         IEnumerable<TDestination> Map(IEnumerable<TSource> sourceList, params object[] constructorParameters);
-        TDestination Map(params object[] constructorParameters);
         void Map(ref TDestination destinationObject, ref TSource sourceObject);
-        void Map(TDestination destinationObject);
         void Map(TDestination destinationObject, TSource sourceObject);
         TDestination Map(TSource sourceObject, params object[] constructorParameters);
-        IEnumerable<string> ShowMapping();
+        string ShowMapping();
     }
 }
--- src/Wayless/WaylessMap.cs
+++ src/Wayless/WaylessMap.cs
@@ -12,19 +12,46 @@ namespace Wayless
         where TDestination : class
         where TSource : class
     {
+        /// <summary>
+        /// Stores mapping rules defining what value to apply to which destination property 
+        /// Depending on _ignoreCasing value key is either the true property name or lower case invariant
+        /// </summary>
         private readonly IDictionary<string, IPropertyInfoPair> _mappingDictionary;
 
+        /// <summary>
+        /// Dictionary of discovered destination type properties
+        /// Depending on _ignoreCasing value key is either the true property name or lower case invariant
+        /// </summary>
         private readonly IDictionary<string, IPropertyDetails> _destinationProperties;
+
+        /// <summary>
+        /// Dictionary of discovered source type properties
+        /// /// Depending on _ignoreCasing value key is either the true property name or lower case invariant
+        /// </summary>
         private readonly IDictionary<string, IPropertyDetails> _sourceProperties;
 
         private readonly bool _ignoreCasing;
-        private readonly TSource _sourceObject;
 
+        /// <summary>
+        /// Properties to receive an explicit value  assignment
+        /// object: value to assign
+        /// </summary>
         private IDictionary<string, object> _explicitAssignments;
+
+        /// <summary>
+        /// Destination properties to be excluded from mapping rules
+        /// </summary>
         private IList<string> _skipFields;
 
         private bool _hasAppliedFieldSkipSet;
 
+        /// <summary>
+        /// Create instance of Wayless mapper
+        /// </summary>
+        /// <param name="ignoreCasing">
+        ///     True: Ignores casing during  initial property matchup
+        ///     False: Property name casing affects matching
+        /// </param>
         public WaylessMap(bool ignoreCasing = false)
         {
             _ignoreCasing = ignoreCasing;
@@ -41,31 +68,44 @@ namespace Wayless
             GenerateDefaultMappingDictionary();
         }
 
-        public WaylessMap(TSource sourceObject, bool ignoreCasing = false)
-            : this(ignoreCasing)
-        {
-            _sourceObject = sourceObject;
-        }
-
+        /// <summary>
+        /// Type to map from
+        /// </summary>
         public Type SourceType { get; }
 
+        /// <summary>
+        /// Type to map to
+        /// </summary>
         public Type DestinationType { get; }
 
+        /// <summary>
+        /// Apply mapping rules to  existing instance of object
+        /// </summary>
+        /// <param name="destinationObject">Object to apply mapping rules to</param>
+        /// <param name="sourceObject">Object to read values from</param>
         public void Map(ref TDestination destinationObject, ref TSource sourceObject)
         {
             InternalMap(destinationObject, sourceObject);
         }
 
+        /// <summary>
+        /// Apply mapping rules to  existing instance of object
+        /// </summary>
+        /// <param name="destinationObject">Object to apply mapping rules to</param>
+        /// <param name="sourceObject">Object to read values from</param>
         public void Map(TDestination destinationObject, TSource sourceObject)
         {
             InternalMap(destinationObject, sourceObject);
         }
 
-        public void Map(TDestination destinationObject)
-        {
-            InternalMap(destinationObject, _sourceObject);
-        }
-
+        /// <summary>
+        /// Apply mapping using a collection of source objects. Each object in the 
+        /// source list will be mapped to a corresponding object in the output list
+        /// </summary>
+        /// <param name="sourceList">List of object sto map</param>
+        /// <param name="constructorParameters">Constructor parameters, if any, to be used when 
+        /// creating an instance of the destination object</param>
+        /// <returns>Collection of mapped objects</returns>
         public IEnumerable<TDestination> Map(IEnumerable<TSource> sourceList, params object[] constructorParameters)
         {
             foreach (var sourceObject in sourceList)
@@ -74,14 +114,12 @@ namespace Wayless
             }
         }
 
-        public TDestination Map(params object[] constructorParameters)
-        {
-            var destinationObject = (TDestination)Activator.CreateInstance(DestinationType, constructorParameters);
-            InternalMap(destinationObject, _sourceObject);
-
-            return destinationObject;
-        }
-
+        /// <summary>
+        /// Apply mapping rules in source object
+        /// </summary>
+        /// <param name="sourceObject">Object to read avalues from</param>
+        /// <param name="constructorParameters">Parameters passed to destination object constructor</param>
+        /// <returns>Mapped object</returns>
         public TDestination Map(TSource sourceObject, params object[] constructorParameters)
         {
             var destinationObject = (TDestination)Activator.CreateInstance(DestinationType, constructorParameters);
@@ -91,14 +129,19 @@ namespace Wayless
             return destinationObject;
         }
 
+        /// <summary>
+        /// Create a mapping rule between destination property and 
+        /// </summary>
+        /// <param name="destinationExpression">Expression for property to be set in destination type</param>
+        /// <param name="sourceExpression">Expression for property to be read in source type</param>
+        /// <returns>Current instance of WaylessMap</returns>
         public IWaylessMap<TDestination, TSource> FieldMap(Expression<Func<TDestination, object>> destinationExpression, Expression<Func<TSource, object>> sourceExpression)
         {
             var destinationKey = GetKey(destinationExpression);
-
             var sourceKey = GetKey(sourceExpression);
 
             if (_mappingDictionary.TryGetValue(destinationKey, out IPropertyInfoPair mappingPair))
-            {
+            {// replace existing mapping rule for property if one exists
                 if (_sourceProperties.TryGetValue(sourceKey, out IPropertyDetails fromSource))
                 {
                     var mapping = _mappingDictionary[destinationKey];
@@ -107,6 +150,7 @@ namespace Wayless
             }
             else
             {
+                // add the new mapping rule to mapping dictionary
                 if ((_destinationProperties.TryGetValue(destinationKey, out IPropertyDetails destinationDetails)) &&
                    (_sourceProperties.TryGetValue(sourceKey, out IPropertyDetails sourceDetails)))
                 {
@@ -119,15 +163,23 @@ namespace Wayless
             return this;
         }
 
-        public IWaylessMap<TDestination, TSource> FieldSet(Expression<Func<TDestination, object>> destinationExpressoin, object fieldValue)
+        /// <summary>
+        /// Create mapping rule to explicitly assign value to destination property
+        /// </summary>
+        /// <param name="destinationExpression">Expression for property to be set in destination type</param>
+        /// <param name="fieldValue">Value to assign</param>
+        /// <returns>Current instance of WaylessMap</returns>
+        public IWaylessMap<TDestination, TSource> FieldSet(Expression<Func<TDestination, object>> destinationExpression, object fieldValue)
         {
+            // delay instantiation of dictionary untill rule is required
             if (_explicitAssignments == null)
             {
                 _explicitAssignments = new Dictionary<string, object>();
             }
 
-            var destinationKey = GetKey(destinationExpressoin);
+            var destinationKey = GetKey(destinationExpression);
 
+            // store assignment for evaluation at call to Map()
             if (_explicitAssignments.ContainsKey(destinationKey))
             {
                 _explicitAssignments[destinationKey] = fieldValue;
@@ -140,6 +192,11 @@ namespace Wayless
             return this;
         }
 
+        /// <summary>
+        /// Create mapping rule for properties to be skipped in destination type
+        /// </summary>
+        /// <param name="ignoreAtDestinationExpression">Expression for property to be skipped in destination type</param>
+        /// <returns>Current instance of WaylessMap</returns>
         public IWaylessMap<TDestination, TSource> FieldSkip(Expression<Func<TDestination, object>> ignoreAtDestinationExpression)
         {
             if (_skipFields == null)
@@ -158,13 +215,31 @@ namespace Wayless
             return this;
         }
 
-        public IEnumerable<string> ShowMapping()
+        /// <summary>
+        /// Returns readable mapping rules set in mapper
+        /// </summary>
+        /// <returns></returns>
+        public string ShowMapping()
         {
-            return _mappingDictionary.Values.Select(v => $"{SourceType.Name}.{v.SourceProperty.Name} => {DestinationType.Name}.{v.DestinationProperty.Name}").ToList();
+            var mappingDictionary = _mappingDictionary.Values.Select(v => $"{DestinationType.Name}.{v.DestinationProperty.Name} = {SourceType.Name}.{v.SourceProperty.Name} ").ToList();
+            var explicitAssignments = _explicitAssignments?.Select(x => $"{DestinationType.Name}.{_destinationProperties[x.Key].PropertyInfo.Name} = {x.Value.ToString()}").ToList();
+            var skipAssignment = _skipFields?.Select(x => $"{DestinationType.Name}.{_destinationProperties[x].PropertyInfo.Name} - Skip").ToList();
+
+            List<string> mappingRules = new List<string>();
+            mappingRules.AddRange(mappingDictionary);
+
+            if (explicitAssignments != null)
+                mappingRules.AddRange(explicitAssignments);
+
+            if (skipAssignment != null)
+                mappingRules.AddRange(skipAssignment);
+
+            return string.Join(Environment.NewLine, mappingRules);
         }
 
         #region helpers
 
+        // apply all mapping rules
         private void InternalMap(TDestination destinationObject, TSource sourceObject)
         {
             ApplyFieldSkipToMappingDictionary();
@@ -174,6 +249,7 @@ namespace Wayless
             ApplyExplicitAssignments(destinationObject, sourceObject);
         }
 
+        // apply mapping dictionary
         private void ApplyMappingDictionary(TDestination destinationObject, TSource sourceObject)
         {
             if (_mappingDictionary.Values.Count > 0)
@@ -195,6 +271,7 @@ namespace Wayless
             }
         }
 
+        // apply explicit assignments
         private void ApplyExplicitAssignments(TDestination destinationObject, TSource sourceObject)
         {
             if (_explicitAssignments != null)
@@ -204,7 +281,8 @@ namespace Wayless
                     if (_destinationProperties.TryGetValue(explicitAssignment.Key, out IPropertyDetails propertyDetails))
                     {
                         if (explicitAssignment.Value.GetType() != propertyDetails.PropertyInfo.PropertyType)
-                        {
+                        {// rudementary logic  to check if we need to use a converter
+                         // this can be improved since some types can be implicitly converted
                             SetValueWithConversion(propertyDetails, destinationObject, explicitAssignment.Value);
                         }
                         else
@@ -216,6 +294,7 @@ namespace Wayless
             }
         }
 
+        // remove rules from both mapping dictionary and explicit assignment
         private void ApplyFieldSkipToMappingDictionary()
         {
             if (_hasAppliedFieldSkipSet || _skipFields == null)
@@ -237,6 +316,7 @@ namespace Wayless
             _hasAppliedFieldSkipSet = true;
         }
 
+        // try applying value using basic conversion
         private void SetValueWithConversion(IPropertyDetails destinationProperty, TDestination destinationObject, object value)
         {
             var converter = TypeDescriptor.GetConverter(value.GetType());
@@ -247,7 +327,7 @@ namespace Wayless
             }
         }
 
-
+        // determine if key is properties true name or invariant lower case
         private string GetKey<T>(Expression<Func<T, object>> expression)
             where T : class
         {
@@ -257,6 +337,7 @@ namespace Wayless
             return _ignoreCasing ? propertyName.ToLowerInvariant() : propertyName;
         }
 
+        // create initial mapping dictionary by matching property (key) names
         private void GenerateDefaultMappingDictionary()
         {
             foreach (var destinationDetails in _destinationProperties)
@@ -269,6 +350,7 @@ namespace Wayless
             }
         }
 
+        // create mapping pair
         private IPropertyInfoPair GetPropertyPair(IPropertyDetails destinationDetails, IPropertyDetails sourceDetails)
         {
             var propertyInfoPair = new PropertyInfoPair
