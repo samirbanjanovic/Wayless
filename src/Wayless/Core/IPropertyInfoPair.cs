c48c69a1acc403e7e3dde2d66e99130b465fd9c7
Samir Banjanovic <sbanjanovic@ontrac.com>
2018-01-22 17:47:39 -0500

    Changing mapper to use Delegates for performance, added some minor performance tests

--- src/Wayless/Core/IPropertyInfoPair.cs
+++ src/Wayless/Core/IPropertyInfoPair.cs
@@ -1,8 +1,14 @@
-﻿namespace Wayless
+﻿using System;
+
+namespace Wayless
 {
-    public interface IPropertyInfoPair
+    public interface IPropertyInfoPair        
     {
         IPropertyDetails DestinationProperty { get; set; }
         IPropertyDetails SourceProperty { get; set; }
+
+        Action<object, object> Setter { get; set; }
+
+        Func<object, object> Getter { get; set; }
     }
 }
